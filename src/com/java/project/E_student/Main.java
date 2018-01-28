70b36a72aec2dce09f828941510e23804483c9bb
jedrzej <jedrzej.kapisz@gmail.com>
2018-01-28 13:31:08 +0100

    initial commit

--- src/com/java/project/E_student/Main.java
+++ src/com/java/project/E_student/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Menu test=new Menu();           // note 1: shift + f6 na nazwie zmiennej - tricky
+        Menu test=new Menu();           //note 1: shift + f6 na nazwie zmiennej - tricky
         test.display_control_panel();
     }
 }
