673b186397b0edf9ba7486933514dd9b995617b5
jedrzej <jedrzej.kapisz@gmail.com>
2018-01-28 13:07:58 +0100

    initial commit

---
+++ src/com/java/project/E_student/Main.java
@@ -0,0 +1,10 @@
+package com.java.project.E_student;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Menu test=new Menu();           //note 1: shift + f6 na nazwie zmiennej - tricky
+        test.display_control_panel();
+    }
+}
---
+++ src/com/java/project/E_student/Teacher.java
@@ -0,0 +1,42 @@
+package com.java.project.E_student;
+
+import java.util.Scanner;
+
+public class Teacher extends Subject {
+    private Scanner scanner=new Scanner(System.in);
+    private String surname;
+    private String education_degree="elementary school";
+    private static int teacher_counter=0;
+
+    public Teacher() {
+        System.out.println("Enter surname: ");
+        this.surname = scanner.nextLine();
+        //this.education_degree = scanner.nextLine();
+        teacher_counter++;
+    }
+
+    public static int getTeacher_counter() {
+        return teacher_counter;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEducation_degree() {
+        return education_degree;
+    }
+
+    public void setSurname() {
+        this.surname = scanner.nextLine();
+    }
+
+    public void setEducation_degree() {
+        System.out.println("Enter degree name: ");
+        this.education_degree = scanner.nextLine();
+    }
+
+    public void display_teacher_info(){
+        System.out.println("Surname: "+this.surname+" Degree: "+this.education_degree);
+    }
+}
